d41e3bd2d5491f37cadc892bc6db1fff16680cae
1shakti <shakti26.sj@gmail.com>
2021-11-22 09:39:00 +0530

    changed layout colors and done user search function

--- css_jsfundamentals/p_js2_usersearch.js
+++ css_jsfundamentals/p_js2_usersearch.js
@@ -1,14 +1,17 @@
 var searchInputField = document.getElementById("searchInput");
 
 searchInputField.addEventListener("keyup", function(event){
-    console.log("usernameinput"+searchInput.value);
     let searchQuery = event.target.value.toLowerCase();
     let allNamesDOMCollection = document.getElementsByClassName("name");     
 
     for(let counter = 0; counter < allNamesDOMCollection.length; counter++){
         const currentname = allNamesDOMCollection[counter].textContent.toLowerCase();
-        console.log(currentname);
+        if(currentname.includes(searchQuery)){
+            allNamesDOMCollection[counter].style.display="block";
+        }else{
+            allNamesDOMCollection[counter].style.display="none";
+        }  
     }
     
-})
+});
 
