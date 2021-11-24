23f6262995b2d965891735a027c81c6a6a31aba9
1shakti <shakti26.sj@gmail.com>
2021-11-24 22:31:11 +0530

    done next and prev slide buttons

--- css_jsfundamentals/p_js3_carousel.js
+++ css_jsfundamentals/p_js3_carousel.js
@@ -8,18 +8,29 @@ var carouselBtnprev = document.getElementById("carousel-button-prev");
 carouselBtnnext.addEventListener('click', moveToNextSlide);
 carouselBtnprev.addEventListener('click', moveToPrevSlide);
 
-function moveToNextSlide(){
+function hideAllslides(){
+    for(let slide of slides){
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+    }
+} 
 
-if(slidePosition === totalSlides -1){
+function moveToNextSlide(){
+    hideAllslides();
+if(slidePosition === totalSlides - 1){
     slidePosition = 0;
 }else{
     slidePosition++;
 }
-
-    slides[slidePosition].classList.add("carousel-item-visible")
-     
+    slides[slidePosition].classList.add("carousel-item-visible");     
 }
 
 function moveToPrevSlide(){
-
+    hideAllslides();
+    if(slidePosition === 0){
+       slidePosition = totalSlides - 1;
+    }else{
+       slidePosition--;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");         
 }
