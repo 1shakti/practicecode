41bd422c64484dc46fae76aef79e4c10b7973768
1shakti <shakti26.sj@gmail.com>
2021-11-26 09:52:20 +0530

    working on it

---
+++ css_jsfundamentals/carousel.js
@@ -0,0 +1,17 @@
+const slides = document.getElementsByClassName("carousel-item");
+
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+var carouselBtnnext = document.getElementById("slide-to-next");
+var carouselBtnprev = document.getElementById("slide-to-prev");
+
+carouselBtnnext.addEventListener("click" , moveToNextSlide);
+carouselBtnprev.addEventListener("click" , moveToPrevSlide);
+
+function moveToNextSlide(){
+
+}
+
+function moveToPrevSlide(){
+}
