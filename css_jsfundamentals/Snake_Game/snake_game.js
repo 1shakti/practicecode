2c0c296104d336e34f9c830c80fdab5d001dc5e5
1shakti <shakti26.sj@gmail.com>
2021-12-02 10:15:53 +0530

    added key controls

--- css_jsfundamentals/Snake_Game/snake_game.js
+++ css_jsfundamentals/Snake_Game/snake_game.js
@@ -4,6 +4,8 @@ const score = document.getElementById('score')
 let squares = [];
 let currentSnake = [2,1,0];
 let direction = 1;
+let width = 10;
+
 
 function createGrid() {
     //create 100 of these elements with a for loop
@@ -27,15 +29,34 @@ currentSnake.forEach(index => squares[index].classList.add("snake"));
 
 function move(){
 
-    squares.pop(currentSnake).classList.remove("snake");
     const tails = currentSnake.pop();
-    console.log(tails);
-    console.log(currentSnake);
+    //console.log(tails);
+    //console.log(currentSnake);
     squares[tails].classList.remove("snake");
 
-    currentSnake.unshift(currentSnake[0]+direction);
-    console.log(currentSnake);  
+    currentSnake.unshift(currentSnake[0] + direction);
+    //console.log(currentSnake);  
     squares[currentSnake[0]].classList.add("snake");
 }
 
 move();
+
+let timerId = setInterval(move, 1000);
+
+function control(e){
+    if(e.keyCode === 39){
+        console.log("right pressed");
+        direction = 1;
+    }else if(e.keyCode === 38){
+        console.log("up pressed");
+        direction = -width;
+    }else if(e.keyCode === 37){
+        console.log("left pressed");
+        direction = -1;
+    }else if(e.keyCode === 40){
+        console.log("down pressed");
+        direction = +width;
+    }
+}
+
+document.addEventListener("keyup", control);
