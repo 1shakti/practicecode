3dba16d32a16d9324a150557f69e48bba0d63ac7
1shakti <shakti26.sj@gmail.com>
2021-12-16 22:49:36 +0530

    class and object

--- js_concepts/p_js2.js
+++ js_concepts/p_js2.js
@@ -7,3 +7,35 @@ let cat = new Cat('Cat', 4);
 cat.makeNoise();
 
 console.log(cat.metaData);
+
+class Player {
+    constructor(name, country){
+        this.name = name;
+        this.country = country;
+    }
+    
+    message(){
+        console.log(`${this.name} was born in ${this.country}`);
+    }
+}
+
+class TennisPlayer extends Player {
+    constructor(name, country,age){
+    super(name,country);
+    this.age = age;    
+    }
+    
+    
+    intro(){
+        console.log(`${this.name} is ${this.age} years old and knows how to play Tennis`); 
+    }
+
+    
+}
+
+
+let call = new Player("Messi", "Argentina");
+let msg = call.message();
+
+let callten = new TennisPlayer("Messi", "Argentina",34);
+callten.intro();
