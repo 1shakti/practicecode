70b68b2ac54939d02f326d6c79b0946df46db290
1shakti <shakti26.sj@gmail.com>
2021-12-17 10:26:11 +0530

    js array use

---
+++ js_concepts/p_js3.js
@@ -0,0 +1,31 @@
+function arrayReplace(array, elemToReplace, substitutionElem){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === elemToReplace){
+            array[i] = substitutionElem;
+        }
+    }
+    return array;
+}
+
+
+
+
+
+describe('arrayReplace()', () => {
+    it('adds a border around entire application', () => {
+
+        // arrange
+        const array = [1,2,3];
+        const elemToReplace = 1;
+        const substitutionElem = 3;
+
+        // act
+        const result = arrayReplace(array, elemToReplace, substitutionElem);
+
+        // log
+        console.log("result: ", result);        
+
+        // assert
+        expect(result).toEqual([3, 2, 3]);
+    });
+});
