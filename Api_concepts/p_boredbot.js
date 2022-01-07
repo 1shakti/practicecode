900d7df48bd5967c8774e398c824d7321360a88d
1shakti <shakti26.sj@gmail.com>
2022-01-07 21:50:12 +0530

    created boredbot

---
+++ Api_concepts/p_boredbot.js
@@ -0,0 +1,33 @@
+/**
+Challenge: 
+
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
+
+*/
+
+/**
+Challenge: 
+
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
+*/
+function getActivityIdea() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("idea").textContent = data.activity
+            document.body.classList.add("fun")
+            document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        })
+}
+
+document.getElementById("bored-button").addEventListener("click", getActivityIdea)
