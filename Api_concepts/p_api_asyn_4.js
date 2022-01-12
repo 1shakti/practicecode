cc5db8bda6994ebd66fd26b1ecfec6da8051b905
1shakti <shakti26.sj@gmail.com>
2022-01-12 10:05:26 +0530

    call back functions and promises concept

--- Api_concepts/p_api_asyn_4.js
+++ Api_concepts/p_api_asyn_4.js
@@ -50,7 +50,7 @@ setTimeout(callback, 2000);
  * Part 2: Move the anonymous in-line function to its own, named function
  */
 
- const people = [
+const people = [
     { name: "Jack", hasPet: true },
     { name: "Jill", hasPet: false },
     { name: "Alice", hasPet: true },
@@ -100,19 +100,14 @@ console.log(peopleWithpets)
  * 5. When the loop is over, return the new array
  */
 
- const people = [
-    { name: "Jack", hasPet: true },
-    { name: "Jill", hasPet: false },
-    { name: "Alice", hasPet: true },
-    { name: "Bob", hasPet: false },
-]
 
 
-function firstArray(array, callback){
+function filterArray(array, callback){
 
     const resultingArray = [];
 
     for( item of array){
+
         const shouldbeInclued = callback(item);
         if(shouldbeInclued){
             resultingArray.push(item)
@@ -120,3 +115,74 @@ function firstArray(array, callback){
     }
     return resultingArray;
 }
+
+
+
+const peopleWithPetss = filterArray(people, function(person) {
+    return person.hasPet
+})
+
+console.log(peopleWithPetss);
+
+
+
+//Array .filter and .map
+
+/**
+ * Challenge: method chaining!
+ * 
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ *    - I realize this might feel like busywork, but my intent will make sense soon
+ * 
+ * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
+ */
+
+// document.getElementById("new-deck").addEventListener("click", function() {
+//     console.log("Clicked!")
+// })
+
+const voters = [
+    {name: "Joe", email: "joe@joe.com", voted: true},
+    {name: "Jane", email: "jane@jane.com", voted: true},
+    {name: "Bo", email: "bo@bo.com", voted: false},
+    {name: "Bane", email: "bane@bane.com", voted: false}
+]
+
+
+
+const finalResult = voters.filter(voter => voter.voted).map(voter => voter.email);
+console.log(finalResult);
+
+
+
+
+
+/**
+ * Time to be curious!
+ * 
+ * What would happen if you didn't return `res.json()` 
+ * from the first .then block?
+ * 
+ * What would the next .then() callback receive as its 
+ * parameter if you returned something totally different??
+ */
+
+/**
+ * Challenge:
+ * 
+ * pass the string "World" down to a 3rd .then() block
+ * and log it to the console inside the body of this new
+ * 3rd .then() block
+ */
+
+ fetch("https://apis.scrimba.com/bored/api/activity")
+ .then(function(res) {
+     return "Hello"
+ })
+ .then(function(whatever) {
+     console.log(whatever)
+     return "World"
+ })
+ .then(function(another) {
+     console.log(another)
+ })
