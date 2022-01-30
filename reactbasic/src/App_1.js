0ee19b799433f7c2ac5231937791064400f4ed87
1shakti <shakti26.sj@gmail.com>
2022-01-30 22:35:16 +0530

    creating layout using react

---
+++ reactbasic/src/App_1.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./components/Navbar_1"
+import Hero from "./components/Hero_1"
+import Card from "./components/Card_1"
+
+export default function App() {
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Card />
+        </div>
+    )
+}
