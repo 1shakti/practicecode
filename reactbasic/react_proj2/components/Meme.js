70c74cedfde2ab19de9779249942ffcca3017485
1shakti <shakti26.sj@gmail.com>
2022-02-10 10:01:07 +0530

    used state to show meme image

--- reactbasic/react_proj2/components/Meme.js
+++ reactbasic/react_proj2/components/Meme.js
@@ -1,6 +1,25 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    const [memeImage , setMemeImage] = React.useState("")
+    
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
+    function getMemeImage(){
+    const  memeArray = memesData.data.memes;
+    const  randomNumber = Math.floor(Math.random() * memeArray.length)
+    setMemeImage(memeArray[randomNumber].url)
+
+    }
+
+
+
     return (
         <main>
             <form className="form">
@@ -20,6 +39,7 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </form>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
