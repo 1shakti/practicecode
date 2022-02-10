6bd032096bec5e4d7c9481d77360a9cf41790cdc
1shakti <shakti26.sj@gmail.com>
2022-02-10 09:12:06 +0530

    using call back function to store prev val

--- reactbasic/statecounter_pract/App.js
+++ reactbasic/statecounter_pract/App.js
@@ -4,12 +4,39 @@ export default function App() {
     /**
      * Challenge: 
      * Add functionality to the minus button
+    
+    */
+        /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
      */
+
+        
     const [count, setCount] = React.useState(0)
     
+    /*using call back function**/
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    /*using call back function**/    
+
     function add() {
         setCount(count + 1)
     }
+
+
     
     function subtract() {
         setCount(count - 1)
