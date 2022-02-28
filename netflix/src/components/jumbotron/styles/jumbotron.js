f505f08cbc46484ee675ea2653f64d6430d54579
1shakti <shakti26.sj@gmail.com>
2022-02-28 10:05:40 +0530

    netflix- worked on jumbotron components

---
+++ netflix/src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,26 @@
+import styled from "styled-components/macro";
+
+export const Inner = styled.div`
+
+display: flex;
+align-item: center;
+flex-direction: ${({ direction }) => direction };
+justify-content: center;
+max-width: 1100px;
+margin: auto;
+width: 100%;
+
+@media(max-width: 1000px){
+    flex-direction: column;
+}
+`;
+
+export const Container = styled.div``;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h2``;
+
+export const Pane = styled.div``;
+
+export const Image = styled.img``;
