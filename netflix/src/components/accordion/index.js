8fee903787612e19ba080b89062fc67208ce54e2
1shakti <shakti26.sj@gmail.com>
2022-03-06 22:34:37 +0530

    working on router and done FAQs

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -31,10 +31,13 @@ Accordion.Item = function AccordionItem({children, ...restProps}){
 
 Accordion.Header = function AccordionHeader({children, ...restProps}){
     const { toggleShow, setToggleShow } = useContext(ToggleContext);
-    return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>{children}</Header>
+    return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+        {children}
+        { toggleShow ? (<img src="/images/icons/close-slim.png" alt="Close" />) : (<img src="/images/icons/add.png" alt="Open"/>) }
+    </Header>
 };
 
-Accordion.body = function AccordionBody({children, ...restProps}){
+Accordion.Body = function AccordionBody({children, ...restProps}){
     const { toggleShow } = useContext(ToggleContext);
     return toggleShow ? <Body {...restProps}>{children}</Body> : null;
 }
--- netflix/src/containers/faqs.js
+++ netflix/src/containers/faqs.js
@@ -1,2 +1,30 @@
 import React from "react";
-import faqsData from "./fixtures/faqs"
+import Accordion from "../components/accordion";
+import OptForm from "../components/opt-form";
+import faqsData from "../fixtures/faqs";
+
+export function FaqsContainer(){
+
+    return(
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            {faqsData.map((item)=>(
+                <Accordion.Item key={item.id}>
+                    <Accordion.Header>{item.header}</Accordion.Header>
+                    <Accordion.Body>{item.body}</Accordion.Body>
+                </Accordion.Item>
+            ))}
+            <Accordion.Item />
+
+            <OptForm>
+                <OptForm.Input Placeholder="Email address" />
+                <OptForm.Button>Try It Now</OptForm.Button>
+                <OptForm.Text>
+                    Ready to watch? Enter your email to create or restart your membership
+                </OptForm.Text>
+            </OptForm>    
+
+        </Accordion>
+    )
+
+}
